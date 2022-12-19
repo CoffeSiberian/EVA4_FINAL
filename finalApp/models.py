f28c76f97ca0f0b5d42dc750d994c63406e3235b
Fernando Garrido <htf754@gmail.com>
2022-12-19 10:46:51 -0300

    base code

---
+++ finalApp/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class reserva(models.Model):
+    nombreResponsable = models.CharField(max_length=100)
+    telefono = models.CharField(max_length=50)
+    fechaReserva = models.DateField()
+    horaReserva = models.DateTimeField()
+    cantidadPersonas = models.IntegerField()
+    estado = models.CharField(max_length=90)
+    observacion = models.CharField(max_length=1000)
