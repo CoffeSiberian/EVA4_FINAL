07e945e3cdb86320725fc24592bdb8a6b341e604
SiberianCoffe <siberiancoffe@outlook.cl>
2022-12-20 00:08:49 -0300

    data required

--- finalApp/models.py
+++ finalApp/models.py
@@ -3,10 +3,10 @@ from django.db import models
 # Create your models here.
 
 class Reserva(models.Model):
-    nombreResponsable = models.CharField(max_length=100)
-    telefono = models.CharField(max_length=50)
-    fechaReserva = models.DateField()
-    horaReserva = models.DateTimeField()
-    cantidadPersonas = models.IntegerField()
-    estado = models.CharField(max_length=90)
+    nombreResponsable = models.CharField(max_length=100, null=False)
+    telefono = models.CharField(max_length=50, null=False)
+    fechaReserva = models.DateField(null=False)
+    horaReserva = models.DateTimeField(null=False)
+    cantidadPersonas = models.IntegerField(null=False)
+    estado = models.CharField(max_length=90, null=False)
     observacion = models.CharField(max_length=1000)
